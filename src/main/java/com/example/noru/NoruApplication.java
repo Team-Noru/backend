baad692c232d02d437513cf2af14e896f2a9bc09
jiminseon <min7un3@gmail.com>
2025-12-05 19:09:58 +0900

    initial commit

---
+++ src/main/java/com/example/noru/NoruApplication.java
@@ -0,0 +1,13 @@
+package com.example.noru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoruApplication.class, args);
+	}
+
+}
