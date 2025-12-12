63f21ea76b2a5bddb951a90539968d12f0be9b4f
jiminseon <min7un3@gmail.com>
2025-12-12 22:07:27 +0900

    modify : 스케줄러 및 RateLimiter 추가 #4

--- src/main/java/com/example/noru/price/service/PriceScheduler.java
+++ src/main/java/com/example/noru/price/service/PriceScheduler.java
@@ -1,9 +1,13 @@
 package com.example.noru.price.service;
 
+import com.example.noru.company.rds.service.CompanyService;
 import com.example.noru.price.domain.StockCode;
+import com.google.common.util.concurrent.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,34 +16,46 @@ public class PriceScheduler {
     private final TokenService tokenService;
     private final PriceFetchService fetchService;
     private final PriceRedisService priceRedisService;
+    private final CompanyService companyService;
 
     private boolean isRunning = false;
 
-    @Scheduled(cron = "0 * * * * *")
-    public void updateAllPrices() {
+    // 초당 3.3개 호출 → 약 0.3초 간격
+    private final RateLimiter rateLimiter = RateLimiter.create(3.3);
 
-        if (isRunning) {
-            System.out.println("⏳ PriceScheduler is already running. Skipped.");
-            return;
-        }
+    // 09:00 ~ 14:59
+    @Scheduled(cron = "0 * 9-14 * * *")
+    public void updateMorning() {
+        runScheduler();
+    }
 
+    // 15:00 ~ 15:29
+    @Scheduled(cron = "0 0-29 15 * * *")
+    public void updateAfternoon() {
+        runScheduler();
+    }
+
+    public void runScheduler() {
+
+        if (isRunning) return;
         isRunning = true;
 
         try {
             String token = tokenService.getAccessToken();
+            List<String> stockCodes = companyService.getDomesticListedCompanies();
 
-            for (String companyId : StockCode.CODES) {
+            for (String companyId : stockCodes) {
+
+                // ⏱ API 호출 속도 조절 (0.3초 간격)
+                rateLimiter.acquire();
+
                 try {
                     String json = fetchService.fetchPrice(token, companyId);
-
-
                     priceRedisService.savePrice(companyId, json);
 
                 } catch (Exception e) {
                     System.out.println("❌ Error fetching " + companyId + ": " + e.getMessage());
                 }
-
-                try { Thread.sleep(300); } catch (InterruptedException ignore) {}
             }
 
         } finally {
