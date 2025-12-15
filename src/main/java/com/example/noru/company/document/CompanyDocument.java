af12f10c65370294a97641e3c4627ff3fe9c26c2
slay1379 <65012092+slay1379@users.noreply.github.com>
2025-12-15 10:35:13 +0900

    Update CompanyDocument.java

--- src/main/java/com/example/noru/company/document/CompanyDocument.java
+++ src/main/java/com/example/noru/company/document/CompanyDocument.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
 @Builder
